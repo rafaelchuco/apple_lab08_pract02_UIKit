9623b7033b08eb39f7291bf0d9c19b94bef8bbe7
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2026-05-04 11:44:16 -0500

    Elimina la funcionalidad de compartir resultados en ResultadoController y ajusta la interfaz en Main.storyboard para mejorar la disposición de los elementos.

--- apple_lab08_pract02_UIKit/ResultadoController.swift
+++ apple_lab08_pract02_UIKit/ResultadoController.swift
@@ -13,7 +13,6 @@ class ResultadoController: UIViewController {
     @IBOutlet weak var primerNumeroLabel: UILabel?
     @IBOutlet weak var segundoNumeroLabel: UILabel?
     @IBOutlet weak var resultadoLabel: UILabel?
-    @IBOutlet weak var compartirButton: UIButton?
     @IBOutlet weak var nuevoCalculoButton: UIButton?
     
     var calculo: Calculo?
@@ -36,29 +35,7 @@ class ResultadoController: UIViewController {
     }
     
     func configurarBotones() {
-        compartirButton?.setTitle("Share", for: .normal)
         nuevoCalculoButton?.setTitle("New Calculation", for: .normal)
-    }
-    
-    @IBAction func compartirTapped(_ sender: UIButton) {
-        guard let calculo = calculo else { return }
-        
-        // ✅ Símbolo dinámico según la operación
-        let simbolo: String
-        
-        switch calculo.operacion {
-        case .suma:
-            simbolo = "+"
-        case .resta:
-            simbolo = "-"
-        case .multiplicacion:
-            simbolo = "×"
-        }
-        
-        let mensaje = "\(Int(calculo.primerNumero)) \(simbolo) \(Int(calculo.segundoNumero)) = \(Int(calculo.resultado))"
-        
-        let actividadVC = UIActivityViewController(activityItems: [mensaje], applicationActivities: nil)
-        present(actividadVC, animated: true)
     }
     
     @IBAction func nuevoCalculoTapped(_ sender: UIButton) {
